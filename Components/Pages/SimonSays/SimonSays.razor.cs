cf3b5e7e5d7d4058c90f2f35a9583c5fc694de28
Augustas Kniška <kniskiukas@gmail.com>
2024-09-29 20:08:32 +0300

    Ported css part to a different file, added SimonSays.razor.cs and SimonSays.razor.css files

---
+++ Components/Pages/SimonSays/SimonSays.razor.cs
@@ -0,0 +1,12 @@
+
+namespace PSInzinerija1.Components.Pages.SimonSays
+{
+    // public partial class SimonSays
+    // {
+    //     GameManager Manager {get;} = new GameManager();
+    //     protected override async Task OnInitializedAsync()
+    //     {
+    //         await Manager.StartNewGame();
+    //     }
+    // }
+}

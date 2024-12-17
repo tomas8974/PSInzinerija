f77f6dc6546bd7036ec8898d5aa50a773384b80a
Mykolas Budrys <mykolas.budrys@gmail.com>
2024-12-17 02:48:21 +0200

    changed login and logout behaviour to use javascript interop

--- Frontend/wwwroot/js/scripts.js
+++ Frontend/wwwroot/js/scripts.js
@@ -9,3 +9,47 @@ function getCookie(name) {
     let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     return match ? match[2] : null;
 }
+
+async function postVoidAsync(url) {
+    $.ajax({
+        url: url, // Replace with your API URL
+        type: 'POST',
+        contentType: 'application/json',  // Set the content type to JSON
+        xhrFields: {
+            withCredentials: true  // Allow cookies to be sent with the request
+        },
+        success: function (response) {
+            console.log("Logout successful:", response);
+        },
+        error: function (xhr, status, error) {
+            console.error("Logout failed:", status, error);
+        }
+    });
+}
+
+async function postLogin(url, email, password) {
+    const formData =
+    {
+        email: email,
+        password: password
+    };
+
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData),
+        credentials: "include"
+    }).then(response => {
+        // Check if response status is successful (2xx)
+        if (response.ok) {
+            return true;  // If the request is successful
+        } else {
+            return false;  // If the request failed
+        }
+    }).catch(error => {
+        console.error("Request failed", error);
+        return false;  // If there is an error with the fetch itself
+    });
+}
